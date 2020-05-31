7cb936af9d82e4ed02128dbb0789ebbf305d6cde
Sarin Wanichwasin <sarin_beam30@hotmail.com>
2020-05-31 18:04:46 +0700

    Updates

---
+++ precache-manifest.ed338fa0b2a9c0ae5d17b01dabc1a70d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f59af804850a56017bfef88f4d03bda",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/index.html"
+  },
+  {
+    "revision": "9af7259c9e276b2aa5f8",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/css/2.f1d904fc.chunk.css"
+  },
+  {
+    "revision": "d1a2b1311ca751e8b461",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/css/main.993ffef9.chunk.css"
+  },
+  {
+    "revision": "9af7259c9e276b2aa5f8",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/js/2.f8ebf89b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/js/2.f8ebf89b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a2b1311ca751e8b461",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/js/main.027ed13e.chunk.js"
+  },
+  {
+    "revision": "7929328c15be9ec6cf30",
+    "url": "/WATER_LEVEL_FRONTEND_REACT/static/js/runtime-main.58f47279.js"
+  }
+]);
